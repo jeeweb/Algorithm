e288e94bcdea6b3016f93aa3eb0babcc6ef4a7db
Jiee Kong <52396673+jeeweb@users.noreply.github.com>
2023-08-08 13:24:19 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+input.sort((a, b) => b - a);
+
+const a = Number(input[0]);
+const b = Number(input[1]);
+const c = Number(input[2]);
+
+if (a == b && b == c) {
+  console.log(10000 + a * 1000);
+} else if ((a == b && a != c) || (a == c && a != b)) {
+  console.log(1000 + a * 100);
+} else if (b == c && a != b) {
+  console.log(1000 + b * 100);
+} else if (a != b && b != c && a != c) {
+  console.log(Math.max(...input) * 100);
+}
