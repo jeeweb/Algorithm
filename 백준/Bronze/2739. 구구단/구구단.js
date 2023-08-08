952ed47e53de46078e8ef7cf22579971a7ba83a4
Jiee Kong <52396673+jeeweb@users.noreply.github.com>
2023-08-08 13:46:24 +0900

    [Bronze V] Title: 구구단, Time: 148 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+let num = Number(input);
+
+for (let i = 1; i < 10; i++) {
+  console.log(`${num} * ${i} = ${num*i}`)
+}
