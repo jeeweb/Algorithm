5a9c7ec2440226f0a9b61cffc2be63dbff59e61e
Jiee Kong <52396673+jeeweb@users.noreply.github.com>
2024-04-14 23:36:02 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,9 +1,6 @@
 function solution(a, b) {
-    let answer = 0;
-    let num1 = parseInt(String(a) + String(b))
-    let num2 = parseInt(String(b) + String(a))
-    
-    num1 - num2 > 0 ? answer = num1 : answer = num2
-    
+    let num1 = Number(`${a}${b}`)
+    let num2 = Number(`${b}${a}`)
+    let answer = Math.max(num1, num2)
     return answer;
 }
