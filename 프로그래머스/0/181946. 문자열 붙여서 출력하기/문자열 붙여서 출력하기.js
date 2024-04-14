7eb45e4eeb5c9501508a60c6ca2e4ee09f8861e6
Jiee Kong <52396673+jeeweb@users.noreply.github.com>
2024-04-14 18:16:23 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 55.93 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -9,7 +9,5 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    str1 = input[0];
-    str2 = input[1];
-    console.log(str1+str2)
+    console.log(input.join(""))
 });
