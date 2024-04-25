dba1260f65dd1ceb90c5610754f520744f664032
Jiee Kong <52396673+jeeweb@users.noreply.github.com>
2024-04-26 02:01:39 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -1,10 +1,4 @@
 function solution(num_list) {
-    const answer = [];
-    while (answer.length < 5) {
-        let minNum = Math.min(...num_list)
-        let index = num_list.indexOf(minNum);
-        num_list.splice(index, 1)
-        answer.push(minNum)
-    }
+    const answer = num_list.sort((a, b) => a - b).slice(0, 5)
     return answer;
 }
