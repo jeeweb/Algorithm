4de3ff665eda5bbf31c50f1bc925cdb12af80006
Jiee Kong <52396673+jeeweb@users.noreply.github.com>
2024-04-27 16:03:41 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    let answer = 1;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] !== arr[j][i]) {
+                return 0;
+            }
+        }
+    }
+    return answer;
+}
