4fe7cac2a0abc0ec5383028367e53f8fc00fe96e
Jiee Kong <52396673+jeeweb@users.noreply.github.com>
2024-05-13 23:46:03 +0900

    [level 2] Title: 주식가격, Time: 4.99 ms, Memory: 42.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,14 @@
+function solution(prices) {
+    const answer = [];
+    for (let i = 0; i < prices.length; i++) {
+        let count = 0;
+        for (let j = i + 1; j < prices.length; j++) {
+            count++;
+            if (prices[i] > prices[j]) {
+                break;
+            }
+        }     
+        answer.push(count);
+    }
+    return answer;
+}
