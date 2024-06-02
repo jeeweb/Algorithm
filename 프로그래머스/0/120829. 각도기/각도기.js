ab4cd1fe74e732a8c4241cb2804a1f36b5295203
Jiee Kong <52396673+jeeweb@users.noreply.github.com>
2024-06-02 18:09:05 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,6 @@
+function solution(angle) {
+    if (angle > 0 && angle < 90) return 1;
+    if (angle === 90) return 2;
+    if (angle > 90 && angle < 180) return 3;
+    if (angle === 180) return 4;
+}
